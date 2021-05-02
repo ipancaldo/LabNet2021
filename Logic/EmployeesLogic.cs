1f139a8244a81c36789b837b3818435016729284
ipancaldo <ignacio_pancaldo@live.com>
2021-05-02 17:17:47 -0300

    Subiendo TP5 desde GIT BASH

---
+++ Logic/EmployeesLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using Data;
+
+namespace Logic
+{
+    public class EmployeesLogic : BaseLogic, IABMLogic<Employees>
+    {
+        public List<Employees> GetAll()
+        {
+            return context.Employees.ToList();
+        }
+
+        public int GetLastID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employees GetData(int id)
+        {
+            Employees employee = new Employees();
+            var seleccionEmpleado = context.Employees.FirstOrDefault(e => e.EmployeeID == id); // throws an exception if producto does not exist
+            return seleccionEmpleado;
+        }
+
+        public void Add(Employees newObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employees objectSelected)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
